18d1bc210ed45191c5adf9e50f8912a81866738b
Vinicius Gonçalves <81978099+ViniiciusGoncalves@users.noreply.github.com>
2023-05-19 14:36:58 -0300

    Projeto da sprint 2

---
+++ src/main/java/com/challenge/challengeapi/controller/MessageController.java
@@ -0,0 +1,65 @@
+package com.challenge.challengeapi.controller;
+
+import com.challenge.challengeapi.entity.Message;
+import com.challenge.challengeapi.entity.UserIdRequest;
+import com.challenge.challengeapi.service.MessageService;
+import com.theokanning.openai.OpenAiService;
+import com.theokanning.openai.completion.CompletionRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/hearmeout/messages")
+public class MessageController {
+    private final MessageService messageService;
+
+    @Autowired
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @PostMapping("/send")
+    public ResponseEntity<String> sendMessage(@RequestBody Message message) {
+        try {
+            messageService.sendMessage(message.getUserId(), message.getText());
+
+            OpenAiService service = new OpenAiService("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");
+            CompletionRequest completionRequest = CompletionRequest.builder()
+                    .prompt(message.getText())
+                    .model("ada")
+                    .build();
+            String response;
+
+            try {
+                response = service.createCompletion(completionRequest).getChoices().get(0).getText();
+            } catch (Exception e) {
+                System.err.println("Erro ao chamar a API do GPT: " + e.getMessage());
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao chamar a API do GPT");
+            }
+
+            System.out.println("Resposta do GPT: " + response);
+
+            message.setResponse(response);
+            messageService.saveMessage(message);
+
+            return ResponseEntity.ok("Mensagem enviada com sucesso.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro na mensagem");
+        }
+    }
+
+    @GetMapping("/history")
+    public ResponseEntity<List<Message>> getMessageHistory(@RequestBody UserIdRequest request) {
+        try {
+            List<Message> history = messageService.getMessageHistory(request.getUserId());
+            return ResponseEntity.ok(history);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+}
+
